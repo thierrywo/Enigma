a5b738dd35b0df6244fe907fd7a684c65419cca9
vriezo <45482682+vriezo@users.noreply.github.com>
2020-06-16 11:59:51 +0200

    Update main.py
    Rotoren toegevoegd + print en input

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 # maak hier de hele enigma machine in python :D
 alfabet = "abcdefghijklmnopqrstuvwxyz"
+
+Rotor1 = "joqrahcldzuvefgikmnstwbxpy" 
+Rotor2 = "gzanbocdthiujkympzersvfwxq" 
+Rotor3 = "kaefgjlmocpbyqrstuvhwdxizn"
+
+print("Welke boodschap wil je encrypten?")
+input()
