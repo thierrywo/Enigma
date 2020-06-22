aac142d09007b61cf3f3eed5c43c71ecbf71bfa6
fimmedevreeze <45490744+fimmedevreeze@users.noreply.github.com>
2020-06-22 16:15:05 +0200

    Definition Rotor veranderd

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # maak hier de hele enigma machine in python :D
 alfabet = "abcdefghijklmnopqrstuvwxyz"
 
-Rotor1 = "joqrahcldzuvefgikmnstwbxpy" 
-Rotor2 = "gzanbocdthiujkympzersvfwxq" 
-Rotor3 = "kaefgjlmocpbyqrstuvhwdxizn"
+Rotor1 = "alfabet" 
+Rotor2 = "alfabet" 
+Rotor3 = "alfabet"
 
 print("Welke boodschap wil je encrypten?")
 input()
