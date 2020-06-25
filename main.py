95b7f5837eb56596a04c649defc09362df31a712
vriezo <45482682+vriezo@users.noreply.github.com>
2020-06-25 20:55:17 +0200

    Update main.py
    Er is nu tekst toegevoegd om te vragen naar de setting van de rotoren en dan kan de gebruiken een nummer invullen

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 # maak hier de hele enigma machine in python :D
 alfabet = "abcdefghijklmnopqrstuvwxyz"
-
-Rotor1 = "alfabet" 
-Rotor2 = "alfabet" 
-Rotor3 = "alfabet"
+rotoren = [] 
+Encryptedmessage = []
 
 print("Welke boodschap wil je encrypten?")
-input()
+tecoderenzin = input().lower
+
+print("Welke setting zou je willen voor de rotoren")
+
+while True:
+  print("Setting voor rotor nummero 1") 
+  settingrot1 = input()
+  if settingrot1 <= 0 or settingrot1 >=25: 
+    break 
+  else: 
+    print("He is niet zo moeilijk. Je hoeft alleen maar een nummer in te vullen en je mag zelfs nummer 1 tot 26 kiezen")
+
+while True:
+  print("Setting voor rotor nummero 2") 
+  settingrot2 = input()
+  if settingrot2 <= 0 or settingrot2 >=25: 
+    break 
+  else: 
+    print("He is niet zo moeilijk. Je hoeft alleen maar een nummer in te vullen en je mag zelfs nummer 1 tot 26 kiezen")
+
+while True:
+  print("Setting voor rotor nummero 3") 
+  settingrot3 = input()
+  if settingrot3 <= 0 or settingrot3 >=25: 
+    break 
+  else: 
+    print("He is niet zo moeilijk. Je hoeft alleen maar een nummer in te vullen en je mag zelfs nummer 1 tot 26 kiezen")
+
