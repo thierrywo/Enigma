2bd56f0aed28044e3f9c64a605dd185ef09261a4
fimmedevreeze <45490744+fimmedevreeze@users.noreply.github.com>
2020-06-26 00:47:20 +0200

    Invullen rotorsettings compacter gemaakt

--- main.py
+++ main.py
@@ -1,42 +1,32 @@
-# maak hier de hele enigma machine in python :D
 alfabet = "abcdefghijklmnopqrstuvwxyz"
 rotoren = [] 
 Encryptedmessage = []
 
+def Rotorsettings():
+  print("Voer de standen van de rotoren in.")
+  for x in range(3):
+    while True:
+      print("Welke setting wilt u voor rotor numero {}?".format(x+1))
+      try:
+       rotoren.append([alfabet,int(input())]) #hij pakt nu oook getallen boven 26
+       break
+      except:
+        print("Het is niet zo moeilijk. Je hoeft alleen maar een nummer in te vullen en je mag zelfs nummer 1 tot 26 kiezen")
+
 print("Welke boodschap wil je encrypten?")
 tecoderenzin = input().lower
 
-print("Welke setting zou je willen voor de rotoren")
+Rotorsettings()
 
-while True:
-  print("Setting voor rotor nummero 1") 
-  settingrot1 = input()
-  if settingrot1 <= 0 or settingrot1 >=25: 
-    break 
-  else: 
-    print("Het is niet zo moeilijk. Je hoeft alleen maar een nummer in te vullen en je mag zelfs nummer 1 tot 26 kiezen")
 
-while True:
-  print("Setting voor rotor nummero 2") 
-  settingrot2 = input()
-  if settingrot2 <= 0 or settingrot2 >=25: 
-    break 
-  else: 
-    print("Het is niet zo moeilijk. Je hoeft alleen maar een nummer in te vullen en je mag zelfs nummer 1 tot 26 kiezen")
 
-while True:
-  print("Setting voor rotor nummero 3") 
-  settingrot3 = input()
-  if settingrot3 <= 0 or settingrot3 >=25: 
-    break 
-  else: 
-    print("Het is niet zo moeilijk. Je hoeft alleen maar een nummer in te vullen en je mag zelfs nummer 1 tot 26 kiezen")
 
+#Afsluiting
 while True: 
   antwoord = input("Nog een boodschap encrypten? (Vul in ja of nee)")
-  if antwoord.lower("ja"): 
-    break 
-  
-  elif antwoord.lower("nee"): 
+  if antwoord.lower() == "ja" : 
+   continue 
+  # hij moet nog returnen naar start
+  elif antwoord.lower() == "nee" :
     print("Nou dan niet hoor, tieft dan maar een pleurisend op")
-    exit 
+    break 
