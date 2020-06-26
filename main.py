a3aa0a244f5d3d10c7ac50dc8b8abac323fecc60
fimmedevreeze <45490744+fimmedevreeze@users.noreply.github.com>
2020-06-26 02:53:00 +0200

    Invullen Rotors, Encrypten
    Pff, het encrypten werkt nu soort van beetje, alleen bij eigenlijk alle input dingen crasht die snel als je iets verkeerts invult. Voor de rest ziet het er ook niet netjes uit. Nu moeten we ook nog decrypten dacht ik.

--- main.py
+++ main.py
@@ -1,25 +1,71 @@
 alfabet = "abcdefghijklmnopqrstuvwxyz"
-rotoren = [] 
-Encryptedmessage = []
 
-def Rotorsettings():
-  print("Voer de standen van de rotoren in.")
-  for x in range(3):
-    while True:
-      print("Welke setting wilt u voor rotor numero {}?".format(x+1))
-      try:
-       rotoren.append([alfabet,int(input())]) #hij pakt nu oook getallen boven 26
-       break
-      except:
-        print("Het is niet zo moeilijk. Je hoeft alleen maar een nummer in te vullen en je mag zelfs nummer 1 tot 26 kiezen")
+class enigma:
+  Rotor_1 = 0
+  Rotor_2 = 0
+  Rotor_3 = 0
 
-print("Welke boodschap wil je encrypten?")
-tecoderenzin = input().lower
+  def __init__(self, Rotor_1,Rotor_2,Rotor_3) :
+    self.Rotor_a = Rotor_1
+    self.Rotor_b = Rotor_2
+    self.Rotor_c = Rotor_3
+  
+  def rotordraai(self, aantaldraaien): 
+   aantaldraaien = aantaldraaien % 26
+   veranderde_alfabet = list(alfabet)
+   for i in range(aantaldraaien):
+      veranderde_alfabet.insert(0, veranderde_alfabet[-1])
+      veranderde_alfabet.pop(-1)
+   return veranderde_alfabet
+ 
+  def volgende_letter(self, rotor, char) :
+    veranderde_alfabet = self.rotordraai(rotor)
+    letterIndex = alfabet.index(char)
+    return veranderde_alfabet[letterIndex]
 
-Rotorsettings()
+     
+  def rotors_draaien(self, plaats) :
+    self.Rotor_a = self.Rotor_a + 1
+
+    if(plaats % 26 == 0):
+      self.Rotor_b = self.Rotor_b + 1
+
+    if(plaats % (26*26)==0):     
+      self.Rotor_c = self.Rotor_c + 1
+
+  def tekst_input(self, tekst) :
+    tekst = tekst.lower()      
+    gecodeerde_tekst = []
+    plaats = 0  
 
 
+    for char in list(tekst) :
+      if not char in alfabet :
+        gecodeerde_tekst.append(char)
+        continue 
+ 
+      gecodeerde_letter = self.volgende_letter(self.Rotor_a, char)
+      gecodeerde_letter = self.volgende_letter(self.Rotor_b, gecodeerde_letter)  
+      gecodeerde_letter = self.volgende_letter(self.Rotor_c, gecodeerde_letter)  
 
+
+      gecodeerde_tekst.append(gecodeerde_letter)
+    
+      self.rotors_draaien(plaats)
+      plaats = plaats + 1
+  
+    return str.join("", gecodeerde_tekst)  
+
+
+tecoderenzin = input("Welke boodschap wil je encrypten?")
+
+a = int(input("Welke setting wilt u voor rotor numero 1?")) # als je iets verkeerds invult crasht die, pff, die van mijn vorige commit was chiller maar kon daar abc niet in difine 
+b = int(input("Welke setting wilt u voor rotor numero 2?"))
+c = int(input("Welke setting wilt u voor rotor numero 3?"))
+
+x = enigma(a, b, c)
+encrypt = x.tekst_input(tecoderenzin)
+print("Encrypte tekst:", encrypt)
 
 #Afsluiting
 while True: 
@@ -29,4 +75,4 @@ while True:
   # hij moet nog returnen naar start
   elif antwoord.lower() == "nee" :
     print("Nou dan niet hoor, tieft dan maar een pleurisend op")
-    break 
+    exit() 
